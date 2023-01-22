48f1c8e35c381ae0198b1e6b88b5cb27d5d40320
Viktor Moskaltsov <vitek_k_94@list.ru>
2023-01-23 01:47:18 +0300

    comitt

--- Modul 4 Lessons/Lesson 3.py
+++ Modul 4 Lessons/Lesson 3.py
@@ -1,1 +1,11 @@
-print()
+import random
+
+fst_1 = [random.randint(50 , 80) for _ in range(10)]
+scd_2 = [random.randint(30 , 60) for _ in range(10)]
+
+sqad = [('Погиб 'if fst_1[dm] + scd_2[dm] > 100 else 'Выжил ' ) for dm in range(10)]
+
+print(fst_1)
+print(scd_2)
+
+print(sqad)
