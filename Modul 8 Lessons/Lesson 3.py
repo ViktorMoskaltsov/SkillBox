3e5d1bddaf2c6112ebc9bec85e52df36906e09b2
Victor <vitek_k_94@list.ru>
2023-02-13 21:04:12 +0300

    comit

--- Modul 8 Lessons/Lesson 3.py
+++ Modul 8 Lessons/Lesson 3.py
@@ -1,25 +1,15 @@
-site = {
+def foo(x):
+    if x == 0:
+        print("Вызов foo(0) возвращает 0")
+        return 0
+    else:
+        print(f"Вызов foo({x - 1}) начинается и добавляется в стек")
+        new_result = foo(x - 1)
+        print(f"Вызов foo({x - 1}) завершился и удаляется из стека")
+        result = x + new_result
+        return result
 
-    'html': {
-
-        'head': {
-
-            'title': 'Мой сайт'
-
-        },
-
-        'body': {
-
-            'h2': 'Здесь будет мой заголовок',
-
-            'div': 'Тут, наверное, какой-то блок',
-
-            'p': 'А вот здесь новый абзац'
-
-        }
-
-    }
-
-}
-for u in site.values():
-    print(u)
+print(f"Вызов foo(2) начинается и добавляется в стек")
+result = foo(2)
+print(f"Вызов foo(2) завершается и удаляется из стека")
+print("Итоговый ответ — ", result)
