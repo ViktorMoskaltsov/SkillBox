f30bf3dda1f82ce14e0d1917ef512794443ebb16
Victor <vitek_k_94@list.ru>
2023-02-23 01:53:17 +0300

    comit

---
+++ Modul 9 Lesson/Lesson 1.py
@@ -0,0 +1,19 @@
+import os
+
+def print_dir (project) :
+    print('Содержимое ',project)
+    for imn in os.listdir(project):
+        path = os.path.join(project,imn)
+        print('  ',path)
+
+
+
+
+
+
+project_py = 'Python_Basic'
+
+
+path_project = os.path.abspath(os.path.join('..','..',project_py))
+print_dir(path_project)
+
